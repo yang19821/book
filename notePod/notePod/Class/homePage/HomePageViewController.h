c5d402f2ba90ac8646819a53191ae6c34fd9df24
yang <1287937158@qq.com>
2021-01-12 17:59:48 +0800

    [user]         name = #####         email = ############

---
+++ notePod/notePod/Class/homePage/HomePageViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomePageViewController.h
+//  notePod
+//
+//  Created by Choice on 2021/1/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomePageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ notePod/notePod/Class/homePage/HomePageViewController.m
@@ -0,0 +1,31 @@
+//
+//  HomePageViewController.m
+//  notePod
+//
+//  Created by Choice on 2021/1/12.
+//
+
+#import "HomePageViewController.h"
+
+@interface HomePageViewController ()
+
+@end
+
+@implementation HomePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.greenColor;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
