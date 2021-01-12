842ca60febd74995fdd9bfd34ffade401d71eeac
yang <1287937158@qq.com>
2021-01-12 17:35:27 +0800

    [user]         name = #####         email = ############

--- notePod/notePod/AppDelegate.h
+++ notePod/notePod/AppDelegate.h
@@ -6,13 +6,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (readonly, strong) NSPersistentContainer *persistentContainer;
-
-- (void)saveContext;
 
 
 @end
